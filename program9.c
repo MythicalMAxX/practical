a982d5f27aac72353cc13752946e68e404439e3d
Vinamra Yadav <73683711+MythicalMAxX@users.noreply.github.com>
2022-03-14 00:39:40 +0530

    Update program9.c

--- program9.c
+++ program9.c
@@ -5,7 +5,7 @@ void main()
 {
      int mat1[10][10], mat2[10][10], mat[10][10], size, i, j, k;
 
-     printf ("Enter rows or columns: ");
+     printf ("Enter rows and columns: ");
      scanf ("%d", &size);
 
      printf ("\n***Enter matrix 1***\n");
